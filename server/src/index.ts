99345d80f884325a211aebdac7e7b30497417fda
deepak kahar <kahardeepak14@gmail.com>
2026-04-29 23:51:33 +0530

    biome updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,27 @@
-import { Elysia } from "elysia";
-import { openapi, fromTypes } from "@elysia/openapi";
 import { cors } from "@elysia/cors";
+import { openapi } from "@elysia/openapi";
+import { Elysia } from "elysia";
 import * as z from "zod";
 
 const app = new Elysia({ prefix: "/api" })
-  .use(cors({ origin: ["http://localhost:3001"] }))
-  .use(
-    openapi({
-      mapJsonSchema: {
-        zod: z.toJSONSchema,
-      },
-    }),
-  )
-  .get("/", () => {
-    return {
-      msg: "Hello Elysia",
-    };
-  })
-  .get("/stream", function* () {
-    yield "Hello";
-    yield "World";
-  })
-  .listen(3000, ({ hostname, port }) => {
-    console.log(`🦊 Elysia is running at ${hostname}:${port}`);
-  });
+	.use(cors({ origin: ["http://localhost:3001"] }))
+	.use(
+		openapi({
+			mapJsonSchema: {
+				zod: z.toJSONSchema,
+			},
+		}),
+	)
+	.get("/", () => {
+		return {
+			msg: "Hello Elysia",
+		};
+	})
+	.get("/stream", function* () {
+		yield "Hello";
+		yield "World";
+	});
+
+app.listen(3000, ({ hostname, port }) => {
+	console.log(`🦊 Elysia is running at ${hostname}:${port}`);
+});
