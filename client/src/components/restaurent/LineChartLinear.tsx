8c13f5837f4c47429a91515d00cc1310033ae732
list <kahardeepak14@gmail.com>
2026-05-25 01:10:08 +0530

    minor issue fixed

--- client/src/components/restaurent/LineChartLinear.tsx
+++ client/src/components/restaurent/LineChartLinear.tsx
@@ -1,16 +1,8 @@
 "use client";
 
-import { TrendingUp } from "lucide-react";
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   ChartContainer,
   ChartTooltip,
